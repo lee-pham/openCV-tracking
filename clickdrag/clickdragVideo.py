7f0b930a01c7664c147e949aa0b666b29afeb8a2
Lee <lee-pham@users.noreply.github.com>
2016-03-19 03:28:30 -0500

    Create clickdragVideo.py
    Click and drag--video edition

---
+++ clickdrag/clickdragVideo.py
@@ -0,0 +1,47 @@
+import cv2
+
+drawing = False
+ix, iy = 0, 0
+fx, fy = 0, 0
+
+
+def mouse_action(event, x, y, flags, param):
+    global drawing, ix, iy, fx, fy
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+        fx, fy = x, y
+        print('click')
+
+    elif event == cv2.EVENT_MOUSEMOVE and drawing:
+        fx, fy = x, y
+        print(x),
+        print(y)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        print('release')
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+cv2.namedWindow('Preview')
+cv2.setMouseCallback('Preview', mouse_action)
+
+while True:
+    _, img = cap.read()
+
+    if drawing:
+        cv2.rectangle(img, (ix, iy), (fx, fy), (255, 0, 0), 2)
+    else:
+        cv2.rectangle(img, (ix, iy), (fx, fy), (255, 255, 255), 2)
+
+    cv2.imshow('Preview', img)
+
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
